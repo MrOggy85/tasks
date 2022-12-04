3320afa2c685dc364150530ce1eb8eb528d2728d
Oskar Lindgren <2677573+MrOggy85@users.noreply.github.com>
2022-12-05 05:17:13 +0900

    chore: add persistgate

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import onStartup from './core/onStartup';
 import store from './core/redux/store';
+import persistor from './core/redux/persistor';
 
 const container = document.getElementById('root');
 if (!container) {
@@ -18,7 +20,9 @@ root.render(
   <React.StrictMode>
     <Router>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </Router>
   </React.StrictMode>,
